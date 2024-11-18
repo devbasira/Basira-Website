ca98fce5ca06f08db90ed1d2ebc53da3cda60b22
Ghazali <shazebansari2536@gmail.com>
2024-11-18 20:17:12 +0530

    Added animation on dot

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState,useEffect } from "react";
+import React, { useState} from "react";
 import ImageBanner from "./ImageBanner";
 import '../app/globals.css'
 import { motion } from 'framer-motion';
