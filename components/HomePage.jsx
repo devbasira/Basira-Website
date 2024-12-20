7d8cb46268aa1563cc219abc3b4e2703d80dd495
Ghazali <shazebansari2536@gmail.com>
2024-12-20 19:56:28 +0530

    New Branch

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState,useEffect } from 'react'
+import React, { useState } from 'react'
 import { CirclePlus } from 'lucide-react'
 import { motion } from 'framer-motion'
 
