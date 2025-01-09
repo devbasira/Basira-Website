91bbee34a03655f757f918c775516e6338f9a05e
Ghazali Hussain <shazebansari2536@gmail.com>
2025-01-09 14:26:13 +0530

    Fixed animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import HomePage from '@/components/HomePage'
-import Projects from '@/components/Projects'
 export default function Home() {
   return (
     <div className="flex flex-col overflow-x-hidden justify-center">
